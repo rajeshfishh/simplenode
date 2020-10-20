67d2b5568fec99f153c0a8aa79fd8355e80f82a9
rajeshfishh <rajeshfishh@gmail.com>
2020-10-20 21:29:03 +0530

    first change

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to the aws processes hiiii");
+
+});
+
+
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log("Server is up and listening on 3003...")
+})
