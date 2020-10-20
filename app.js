49ba935c9953f79adf1831af80bf61e67eed9731
rajeshfishh <rajeshfishh@gmail.com>
2020-10-20 21:46:16 +0530

    changes done

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to the aws processes hiiii");
+    res.send("Welcome to the aws processes simplenodeConversation suceed2");
 
 });
 
